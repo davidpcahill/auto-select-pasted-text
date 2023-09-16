962add4d38c382931a74cf7a8988a67128bc88fb
TagWolf <furrious@gmail.com>
2023-09-16 02:40:31 -0700

    0.0.3 Release Bug Fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,65 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    vscode.workspace.onDidChangeTextDocument(async (event) => {
-        let isEnabled = vscode.workspace.getConfiguration().get('autoSelectPastedText.enabled', true);
 
-        if (!isEnabled) {
-            // If the extension is disabled, just run the default paste command
-            await vscode.commands.executeCommand('default:paste');
-            return;
-        }
+	// A flag to determine if the last action by the user was a paste action.
+	let hasPastedRecently = false;
 
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
+	// Monitor changes in the text document (like pasting).
+	vscode.workspace.onDidChangeTextDocument(event => {
+		// Only proceed if a paste action was detected previously.
+		if (!hasPastedRecently) {
+			return;
+		}
 
-        // Check for the latest content change
-        let contentChanges = event.contentChanges;
-        if (contentChanges && contentChanges.length > 0) {
-            let contentChange = contentChanges[contentChanges.length - 1]; // Get the last change
+		// Check if the auto-selection feature is enabled in settings.
+		let isEnabled = vscode.workspace.getConfiguration().get('autoSelectPastedText.enabled', true);
+		if (!isEnabled) {
+			return;
+		}
 
-            let start = contentChange.range.start;
-            let end = new vscode.Position(start.line, start.character + contentChange.text.length);
-            
-            // Set the new selection
-            editor.selection = new vscode.Selection(start, end);
-        }
-    });
+		// Get the currently active editor.
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		// Determine the range of the pasted content.
+		let contentChanges = event.contentChanges;
+		if (contentChanges && contentChanges.length > 0) {
+			let contentChange = contentChanges[contentChanges.length - 1];
+			let start = contentChange.range.start;
+			let end = new vscode.Position(start.line, start.character + contentChange.text.length);
+
+			// Highlight (select) the pasted content.
+			editor.selection = new vscode.Selection(start, end);
+		}
+
+		// Reset for future paste actions.
+		hasPastedRecently = false;
+	});
+
+	// Override the default paste action to set our flag and then perform the normal paste.
+	vscode.commands.registerCommand('editor.action.clipboardPasteAction', async () => {
+		hasPastedRecently = true;
+		await vscode.commands.executeCommand('default:paste');
+	});
+
+	// Deselect text if the user changes the active editor or navigates to another file.
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		if (editor) {
+			const position = editor.selection.active;
+			editor.selection = new vscode.Selection(position, position);
+		}
+	});
+
+	// Deselect text if all selected content is deleted.
+	vscode.window.onDidChangeTextEditorSelection(event => {
+		if (event.selections[0].isEmpty) {
+			const position = event.selections[0].active;
+			event.textEditor.selection = new vscode.Selection(position, position);
+		}
+	});
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
