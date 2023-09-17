5bde631503784be99a624e3b991c675097db484d
TagWolf <furrious@gmail.com>
2023-09-17 03:29:42 -0700

    Release of 0.1.2

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,20 @@
 import * as vscode from 'vscode';
 
+// Variable to store the last pasted content
 let lastPastedContent: string | null = null;
 
 export function activate(context: vscode.ExtensionContext) {
+    // Create an output channel for logging extension-related activities
     const outputChannel = vscode.window.createOutputChannel("AutoSelectPaste");
 
+    // Indicate activation in the output channel
     outputChannel.appendLine('Activating AutoSelectPaste extension...');
 
+    // Register the paste command
     let pasteCommandDisposable = vscode.commands.registerCommand('editor.action.clipboardPasteAction', async () => {
         outputChannel.appendLine('Paste command executed.');
 
+        // Read content from clipboard
         const clipboardContent = await vscode.env.clipboard.readText();
 
         if (clipboardContent) {
@@ -17,31 +22,34 @@ export function activate(context: vscode.ExtensionContext) {
             if (editor) {
                 let targetSelection: vscode.Selection;
 
-                // If the clipboard content matches the last pasted content, append after the current selection.
+                // Determine the target for the paste: either append after the current selection or replace it
                 if (clipboardContent === lastPastedContent) {
                     const currentPosition = editor.selection.end;
                     targetSelection = new vscode.Selection(currentPosition, currentPosition);
                 } else {
                     targetSelection = editor.selection;
-                    lastPastedContent = clipboardContent;  // Update the last pasted content
+                    lastPastedContent = clipboardContent;  // Cache the clipboard content
                 }
 
+                // Split content by lines to calculate the selection end position later
                 const linesPasted = clipboardContent.split('\n');
                 const lastLineLength = linesPasted[linesPasted.length - 1].length;
 
                 editor.edit((editBuilder) => {
+                    // Replace the determined target selection with the clipboard content
                     editBuilder.replace(targetSelection, clipboardContent);
                 }).then((success) => {
                     if (success) {
                         outputChannel.appendLine("Content pasted successfully.");
 
-                        // Calculate the end position for the selection after paste
+                        // Determine the end position for the selection post-paste
                         const endLine = targetSelection.start.line + linesPasted.length - 1;
                         const endCharacter = (linesPasted.length === 1) ? (targetSelection.start.character + lastLineLength) : lastLineLength;
                         const endPosition = new vscode.Position(endLine, endCharacter);
 
-                        // Create a new selection that covers the pasted content
+                        // Adjust the selection to cover the pasted content
                         editor.selection = new vscode.Selection(targetSelection.start, endPosition);
+                        // Reveal the pasted content in the editor
                         editor.revealRange(new vscode.Range(targetSelection.start, endPosition), vscode.TextEditorRevealType.Default);
                     } else {
                         outputChannel.appendLine("Failed to paste content.");
@@ -53,9 +61,10 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    // Add the paste command disposable to the extension context's subscriptions
     context.subscriptions.push(pasteCommandDisposable);
 
-    // Log changes to text editor selection for debugging purposes
+    // Debug: Log selection changes in the editor for tracking
     vscode.window.onDidChangeTextEditorSelection((event) => {
         if (event.textEditor.document.uri.scheme === 'file') {
             const selections = event.selections;
